698adcf98e13bb75f22f0b1871798b3956f37e0c
alessandrodaniello105 <kanymsevement@gmail.com>
2023-09-25 16:21:44 +0200

    [add]: dynamic post elements and printing

--- main.js
+++ main.js
@@ -58,89 +58,118 @@ const posts = [
 
 // 1. Commento il post statico nel DOM e definisco nello script il contenitore (.posts-list)
 const postContainer = document.querySelector('.posts-list');
+;
 
-// 2. Creo l'elemento post e lo appendo al contenitore prima definito
-const post = document.createElement('div');
-post.className = 'post';
-postContainer.append(post);
 
-// 3. All'interno del post, utilizzando il template literals, inserisco il post statico
-post.innerHTML = `
-    <div class="post__header">
-
-        <div class="post-meta">                    
-            <div class="post-meta__icon">
-                <img class="profile-pic" src="https://unsplash.it/300/300?image=15" alt="Phil Mangione">                    
-            </div>
-            <div class="post-meta__data">
-                <div class="post-meta__author">Phil Mangione</div>
-                <div class="post-meta__time">4 mesi fa</div>
-            </div>                    
-        </div>
-
-    </div>
-
-    <div class="post__text">Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.</div>
-
-    <div class="post__image">
-        <img src="https://unsplash.it/600/300?image=171" alt="">
-    </div>
-
-    <div class="post__footer">
-
-        <div class="likes js-likes">
-
-            <div class="likes__cta">
-                <a class="like-button  js-like-button" href="#" data-postid="1">
-                
-                    <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
-                    <span class="like-button__label">Mi Piace</span>
-                </a>
-            </div>
-
-            <div class="likes__counter">
-                Piace a <b id="like-counter-1" class="js-likes-counter">80</b> persone
-            </div>
-
-        </div> 
-
-    </div> 
-`;
 
 // 4. Clono l'array 'posts' in 'myPosts' e, destrutturando la data, la organizzo all'americana
 // const myPosts = [...posts];
 const myPosts = posts.map((post) => {
 
     const {id, content, media, author, likes, created} = post;
-    // const dateUS = {
+    // const dateUSList = {
     //     year: 0,
     //     month: 0,
     //     day: 0
     // };
 
     // const dateSplitted = created.split('-');
-    // console.log(dateSplitted)
-    // let baseYear = created[0]
-    // let baseMonth = created[1]
-    // let baseDay = created[2]
 
-    // console.log(baseDay + baseMonth + baseYear)
+    // dateUSList.year = dateSplitted[0];
+    // dateUSList.month = dateSplitted[1];
+    // dateUSList.day = dateSplitted[2];
+
+    // const dateUSOutput = `
+    //     ${dateUSList.month}-${dateUSList.day}-${dateUSList.year}
+    // `; 
+
+    // console.log(dateUSOutput)
+
+    // const {name, image} = author;
+    // console.log(image)
+    // if (!image) {
+    //     return 'alfonso'
+    // }
 
     const dateList = created.split('-');
 
-    const dateIT = dateList[2] + ' - ' + dateList[1] + ' - ' + dateList[0];
-    const dateUS = dateList[1] + ' - ' + dateList[2] + ' - ' + dateList[0];
+    const dateIT = dateList[2] + '-' + dateList[1] + '-' + dateList[0];
+    const dateUS = dateList[1] + '-' + dateList[2] + '-' + dateList[0];
 
-    // console.log(dateUS)
+
     return {
         id,
         author,
+        // dateUSOutput,
         dateUS,
         dateIT,
         content,
         likes,
         media
     }
+    
 });
 
+
+myPosts.forEach((post) => {
+
+    // console.log(post.dateUS)
+
+    const postElement = document.createElement('div');
+    postElement.className = 'post';
+    postContainer.append(postElement)
+
+
+    // 3. All'interno del post, utilizzando il template literals, inserisco il post statico
+    // 5. Sostituisco i dati dinamici agli elementi statici del post (nome autore, foto autore, ...)
+    postElement.innerHTML = `
+    
+        <div class="post__header">
+
+            <div class="post-meta">                    
+                <div class="post-meta__icon">
+                    <img class="profile-pic" src=${post.author.image} alt=${post.author.name}>                    
+                </div>
+                <div class="post-meta__data">
+                    <div class="post-meta__author">Phil Mangione</div>
+                    <div class="post-meta__time">4 mesi fa</div>
+                </div>                    
+            </div>
+
+        </div>
+
+        <div class="post__text">Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.</div>
+
+        <div class="post__image">
+            <img src="https://unsplash.it/600/300?image=171" alt="">
+        </div>
+
+        <div class="post__footer">
+
+            <div class="likes js-likes">
+
+                <div class="likes__cta">
+                    <a class="like-button  js-like-button" href="#" data-postid="1">
+                    
+                        <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                        <span class="like-button__label">Mi Piace</span>
+                    </a>
+                </div>
+
+                <div class="likes__counter">
+                    Piace a <b id="like-counter-1" class="js-likes-counter">80</b> persone
+                </div>
+
+            </div> 
+
+        </div> 
+    `;
+});
+
+
+// 2. Creo l'elemento post e lo appendo al contenitore prima definito
+
+
+
+console.log(myPosts.id)
 console.log(myPosts)
